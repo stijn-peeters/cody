0800cd83e1e6112bdaee2ebc0c86e63372d23f7c
Stijn Peeters <stijn.peeters.it@gmail.com>
2024-10-15 17:52:00 +0200

    scipt test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+
 import { merriweather } from "@/app/fonts/fonts";
 import "./globals.css";
 
@@ -14,6 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="test">
+          {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "oj33yne69z");`}
+        </Script>
+      </head>
       <body className={`${merriweather.className} antialiased`}>
         {children}
       </body>
